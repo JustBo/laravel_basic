0434e4da1e675c8bce092f41e1b392994b76c487
justbo <b.tsybulskyy@gmail.com>
2017-11-06 13:09:31 +0200

    s3l9

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,4 +2,6 @@
 
 @section('content')
   <h1>About</h1>
+  <p>Duis aute irure dolor in reprehenderit in voluptat
+    e velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 @endsection
---
+++ resources/views/inc/showcase.blade.php
@@ -0,0 +1,6 @@
+<div class="jumbotron text-center">
+  <div class="container">
+    <h1>Welcome!</h1>
+
+  </div>
+</div>
